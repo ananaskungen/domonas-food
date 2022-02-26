f499a72f77cae9372dcf612745a420bf42149908
certifiedgenius <mugeera.mansoor@medieinstitutet.se>
2022-02-26 18:27:11 +0100

    feed bootstrap done?

--- resources/views/Posts/index.blade.php
+++ resources/views/Posts/index.blade.php
@@ -2,23 +2,35 @@
 
 @section('content')
 
-@foreach($posts as $post) 
+@foreach($posts as $post)
 
-<div class="col-4">
-  <h3><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h3>
+<div class="container">
+  <div>
 
-  <!-- Check if post ID == user ID -->
-  @if($post->user_id == auth()->id())
-  <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-  @endif
+    <div class="ms-5" class="col-4 d-flex justify-content-center">
+      <h3><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h3>
+      <h3><p>{{ $post->description }}</p></h3>
+      <img src="https://www.thetranshotel.com/images/Y1WCKFz4BX_980x500.jpg" alt="Pakistani Buffe" width="200px" height="200px">
+    </div>
+
+    <div class="btn-group d-flex justify-content-center">
+      <div>
+        <!-- Check if post ID == user ID Button -->
+        @if($post->user_id == auth()->id())
+
+          <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+          @endif
+        </div>
+
+        <div>
+          @if($post->user_id == auth()->id())
+          <a href="/posts/create" class="btn btn-primary ms-2">Create</a>
+          @endif
+        </div>
+      </div>
+  </div>
 </div>
 
 @endforeach
-<div>
-  @if($post->user_id == auth()->id())
-  <a href="/posts/create" class="btn btn-primary">Create</a>
-  @endif
-</div>
-    
-@endsection
 
+@endsection
